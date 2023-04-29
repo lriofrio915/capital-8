d8d8e465b3b7ad71a550ecc352777b765bebc1eb
luisRiofrio <lriofrio915@gmail.com>
2023-04-29 07:20:00 -0400

    creacion del Trend

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import React, { useRef } from "react";
 import Layout from '../components/Layout';
 import Learn from "@/screens/Home/Learn";
 import Main from "../screens/Home/Main";
+import Trend from "../screens/Home/Trend";
 
 export default function Home() {
   
@@ -11,6 +12,7 @@ export default function Home() {
     <Layout>
       <Main scrollToRef={scrollToRef} />
       <Learn scrollToRef={scrollToRef} />
+      <Trend/>
     </Layout>
   );
 }
